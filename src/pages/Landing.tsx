4fd0eb32e83f719c9f69287b90bf18be5da3f954
Isaque <isaque.mion@gmail.com>
2025-09-04 08:26:04 -0300

    fix(ts): remove variáveis não utilizadas

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -14,7 +14,7 @@ export default function Landing() {
   useEffect(() => {
     (async () => {
       setStatus('Conectando...')
-      const { data: { user }, error } = await supabase.auth.getUser()
+      const { data: { user } } = await supabase.auth.getUser()
       if (!user) {
         const { data, error: signErr } = await supabase.auth.signInAnonymously()
         if (signErr) {
