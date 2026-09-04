75d8968d10bd7b002d8d64e08f88ac4b9f23fb5c
Isaque Mion <isaque.mion@gmail.com>
2026-09-04 15:34:52 -0300

    chore: clean project metadata and tooling

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-// src/App.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
-import Swipe from './pages/Swipe'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import JoinRedirect from './pages/JoinRedirect'
+import Landing from './pages/Landing'
 import Matches from './pages/Matches'
+import Swipe from './pages/Swipe'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-         <Route path="/s/:code" element={<Swipe />} />
-        <Route path="/join" element={<JoinRedirect />} /> 
+        <Route path="/join" element={<JoinRedirect />} />
+        <Route path="/s/:code" element={<Swipe />} />
         <Route path="/s/:code/matches" element={<Matches />} />
       </Routes>
     </BrowserRouter>
